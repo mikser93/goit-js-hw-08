b0dfa4e6ded91901356749947eb510d049456f1f
Vova Vykhrystenko <mikserwowa@gmail.com>
2022-08-14 18:08:03 +0300

    add 1th task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
-
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
 console.log(galleryItems);
+const imgCollection = document.querySelector('.gallery');
+const imgMarkup = galleryItems
+  .map(
+    item =>
+      `<div><a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a></div>`
+  )
+  .join('');
+
+imgCollection.insertAdjacentHTML('afterbegin', imgMarkup);
+
+new SimpleLightbox('.gallery a', {
+  //   captions: true,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
