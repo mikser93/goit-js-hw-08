2c2f4d3bb2b0f4300612c3455aedd467a8109979
Vova Vykhrystenko <mikserwowa@gmail.com>
2022-08-15 13:47:22 +0300

    task 3 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('form');
+const email = form.querySelector('input');
+const name = form.querySelector('textarea');
+
+const STORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener('submit', formSubmit);
+form.addEventListener('input', throttle(formInput, 500));
+
+exampleForm();
+
+function formSubmit(event) {
+  event.preventDefault();
+  const formData = {
+    email: email.value,
+    message: name.value,
+  };
+  console.log(formData);
+  localStorage.removeItem(STORAGE_KEY);
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  event.currentTarget.reset();
+}
+
+function formInput() {
+  const formData = {
+    email: email.value,
+    message: name.value,
+  };
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function exampleForm() {
+  const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (savedMessage) {
+    email.value = savedMessage.email;
+    name.value = savedMessage.message;
+  }
+}
